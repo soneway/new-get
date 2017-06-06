fbab8b4df0852452af1836a51e17c2b260cf7b3e
soneway <250084439@qq.com>
2017-06-06 23:35:55 +0800

    feat: cm

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const likeArray = document.querySelectorAll('*');
 const array = [].slice.call(likeArray);
 
-// better
-const array = [...likeArray];
+// new
+const array2 = [...likeArray];
 
 
 let html = '';
@@ -9,5 +10,16 @@ for (let i = 0, len = 10; i < len; i++) {
   html += `<div data-index="${i}"></div>`;
 }
 
-// better
-const html = [...new Array(10)].map((_, i) => `<div data-index="${i}"></div>`).join('');
+// new
+const html2 = [...new Array(10)].map((_, i) => `<div data-index="${i}"></div>`).join('');
+
+
+function foo(a, b) {
+  console.log(a, b);
+}
+function bar(b) {
+  foo(1, b);
+}
+
+// new
+const bar2 = foo.bind(null, 1);
